46694baa690f40fa53928320ea6c4fcf3872472d
Alexis Fox <alexis.fox@duke.edu>
2025-06-09 14:48:26 -0400

    norm

--- scripts/sym-norm.sh
+++ scripts/sym-norm.sh
@@ -5,7 +5,7 @@ N=100000
 
 # 1. Original training (1 epoch)
 python examples/train_symbolic_example.py \
-  --use_reconstruction \
+  --use_standard_norm \
   --use_proj --use_v \
   --batch_size 4 --effective_batch_size 8 \
   --num_epochs 1 --max_samples $N --n_embd 384\
@@ -13,7 +13,7 @@ python examples/train_symbolic_example.py \
 
 # 2. Resume with 2x effective batch (epoch 2)
 python examples/train_symbolic_example.py \
-  --use_reconstruction \
+  --use_standard_norm \
   --use_proj --use_v \
   --resume_from_checkpoint $DIR"/checkpoint_epoch_1.pt" \
   --batch_size 4 --effective_batch_size 32 \
@@ -22,7 +22,7 @@ python examples/train_symbolic_example.py \
 
 # 3. Resume with same effective batch (epoch 3)
 python examples/train_symbolic_example.py \
-  --use_reconstruction \
+  --use_standard_norm \
   --use_proj --use_v \
   --resume_from_checkpoint $DIR"/checkpoint_epoch_3.pt" \
   --batch_size 4 --effective_batch_size 64 \
