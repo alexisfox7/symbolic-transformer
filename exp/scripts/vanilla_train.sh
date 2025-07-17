4a9231d51f032d020084baf53d09c30751306f99
Alexis Fox <alexis.fox@duke.edu>
2025-07-17 14:34:13 -0400

    Fix vanilla training script

--- exp/scripts/vanilla_train.sh
+++ exp/scripts/vanilla_train.sh
@@ -4,7 +4,7 @@
 set -e
 
 # CONFIG
-DIR="./outputs/vanilla_sparse"
+DIR="./outputs/vanilla_reason"
 N=150000
 N_EMBD=384
 PRESET="small"
@@ -48,7 +48,7 @@ accelerate launch \
     --json_log_steps $JSON_LOG_STEPS \
     --clip_grad_norm 1.0 \
     --val_ratio 0.1 \
-    --validate_every 1 \
-    --use_sparsemax
+    --validate_every 1
+    #--use_sparsemax
 
-echo "Training completed!"
+echo "Training completed
