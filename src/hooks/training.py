82af3e5797217be2aad69eaebbe6a6279c7a5e1c
Alexis Fox <alexis.fox@duke.edu>
2025-07-29 23:04:00 -0400

    Sanity check

--- src/hooks/training.py
+++ src/hooks/training.py
@@ -6,7 +6,6 @@ import os
 import json  
 import math
 from datetime import datetime
-# logger imported from src.utils.logger
 
 class ConsoleLogHook(TrainingHook):
     def __init__(self, log_every_n_batches: int):
@@ -36,7 +35,7 @@ class ConsoleLogHook(TrainingHook):
         batch_idx = state.get("current_batch_idx")
         current_epoch = state.get("current_epoch")
         total_epochs = state.get("num_epochs")
-
+        print("HI")
         if batch_idx % self.log_every_n_batches == 0:
             logger.info(f"Epoch {current_epoch}/{total_epochs}, Batch {batch_idx}")
 
